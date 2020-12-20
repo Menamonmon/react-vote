40e9f1bb406464417eca56a72c0e6bb48f32e037
Menamonmon <monyasser111@gmail.com>
2020-12-20 15:31:38 -0500

    Login class-based created. It returns username, state, id with token on login

--- voting_backend/users/accounts/urls.py
+++ voting_backend/users/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegisterView, login_view, LogoutView
+from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', login_view, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- voting_backend/users/accounts/views.py
+++ voting_backend/users/accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authtoken.views import obtain_auth_token as login_view
 
@@ -50,3 +51,20 @@ class LogoutView(APIView):
         Token.objects.create(user=user)
 
         return Response({ 'details': f'{str(user)} is logged out successfully.' }, status=status.HTTP_200_OK)
+
+
+
+class LoginView(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'username': user.username,
+            'state': user.state,
+        })
