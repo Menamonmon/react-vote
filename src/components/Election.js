6afa9c394c62f9af3c8b3be4bac7454fda1aa45b
Menamonmon <monyasser111@gmail.com>
2020-12-21 13:06:27 -0500

    submission and vote editing added to election component

--- src/components/Election.js
+++ src/components/Election.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { submitVote } from "../helpers/requests";
+import { AuthContext } from "../contexts/AuthConext";
 
 export function CandidateBox(props) {
   const { name, party, checked } = props;
@@ -21,10 +23,57 @@ const CurrentCandidate = ({ name }) => (
 export default function Election(props) {
   const { type, id, state, year, candidates } = props;
 
-  let [currentCandidate, setCurrentCandidate] = useState("");
+  const {
+    APIUrl,
+    syncUserData,
+    user: { votes },
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    loadVotes(votes);
+  }, []);
+
+  let [currentCandidate, setCurrentCandidate] = useState({});
   let [checkedCandidates, setCheckedCandidates] = useState(
     Array(candidates.length).fill(false)
   );
+
+  function loadVotes(votes) {
+    for (const vote of votes) {
+      if (vote.election.id === id) {
+        const candidate_id = vote.candidate.id;
+        const matchingCandidates = candidates.filter(
+          (candidate) => candidate.id === candidate_id
+        );
+        if (matchingCandidates.length !== 1) {
+          throw Error("Candidate ID Not Valid");
+        }
+        const candidate = matchingCandidates[0];
+        const candidateIndex = candidates.indexOf(candidate);
+        setCheckedCandidates((p) => {
+          const prevCandidateIndex = candidates.indexOf(currentCandidate);
+          p[prevCandidateIndex] = false;
+          p[candidateIndex] = true;
+          return p;
+        });
+        setCurrentCandidate(candidate);
+
+        return;
+      }
+    }
+  }
+
+  function submitElectionVote() {
+    const data = {
+      election_id: id,
+      candidate_id: currentCandidate.id,
+    };
+    if (Object.values(data).includes(undefined)) return;
+    submitVote(APIUrl, data).then(() => {
+      syncUserData();
+    });
+  }
+
   return (
     <div className="candidates-container" key={id}>
       {type ? (
@@ -36,9 +85,10 @@ export default function Election(props) {
       )}
       <h4 className="title">Choose One of The Candidates Below:</h4>
       <ul className="candidates-list">
-        {candidates.map(({ name, party, id }, index) => {
+        {candidates.map((candidate, index) => {
+          const { name, party, id } = candidate;
           const onClick = () => {
-            setCurrentCandidate(name);
+            setCurrentCandidate(candidate);
             setCheckedCandidates((prevState) => {
               prevState.fill(false);
               prevState[index] = true;
@@ -56,7 +106,15 @@ export default function Election(props) {
           );
         })}
       </ul>
-      <CurrentCandidate name={currentCandidate} />
+      <div className="election-footer">
+        <CurrentCandidate name={currentCandidate.name} />
+        <button
+          className="election-submit-btn form-submit-btn"
+          onClick={submitElectionVote}
+        >
+          Submit Vote
+        </button>
+      </div>
     </div>
   );
 }
