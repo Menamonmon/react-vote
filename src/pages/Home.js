8868b4bcfae107f72b3a92a8bd3c784a11f7f0d6
Menamonmon <monyasser111@gmail.com>
2020-12-24 10:44:50 -0500

    Home page created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default function Home() {
-    return (
-        <div className="container homepage">
-            {}
-        </div>
-    );
+  return (
+    <div className="container homepage">
+      <h2 className="container-title">Welcome To React Vote:</h2>
+      <h3 className="container-subtitle">
+        React Vote is a simple web app that is built using React, Django, and
+        the Django REST Framework. It gives you the opportunity to signup for an
+        account which would then give you access to the elections that match
+        your state. You can view your elections, change your vote, and view your
+        previous votes. This website is built by Mena Filfil. (
+        <a href="https://github.com/menamonmon">visit my GitHub page</a>)
+      </h3>
+    </div>
+  );
 }
