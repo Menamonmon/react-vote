4048d4f7c93e296995cc2ff9e636b932983001b1
Menamonmon <monyasser111@gmail.com>
2020-12-23 22:40:22 -0500

    Results page and component done

---
+++ src/components/ResultBox.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export function CandidateResult({ id, name, party, voteCount }) {
+  return (
+    <div className="candidate-result-box" key={id}>
+      <h6 className="container-button container-subtitle">
+        {`${name} (${party})`}
+        <br />
+        Vote Count: {voteCount}
+      </h6>
+    </div>
+  );
+}
+
+export function ArrowButton({ toggled, onClick }) {
+  return (
+    <div onClick={onClick} className={`arrow-btn ${toggled ? "open" : ""}`}>
+      ⯈
+    </div>
+  );
+}
+
+export function CandidatesResultsContainer({ toggled, candidates }) {
+  return (
+    <div className={`candidates-results-container ${toggled ? "open" : ""}`}>
+      {candidates.map(({ name, party, vote_count }) => (
+        <CandidateResult name={name} party={party} voteCount={vote_count} />
+      ))}
+    </div>
+  );
+}
+
+export default function ResultBox({
+  election: { _type, state, year, candidate_set },
+}) {
+  const [toggled, setToggled] = useState(false);
+  function onToggle() {
+    setToggled((prev) => !prev);
+  }
+
+  return (
+    <div className="container-btn result-box" onClick={onToggle}>
+      <div className="result-box-header">
+        <h4 className="conatiner-subtitle">{`${_type} (${year} ${state})`}</h4>
+        <ArrowButton toggled={toggled} />
+      </div>
+      <CandidatesResultsContainer
+        candidates={candidate_set}
+        toggled={toggled}
+      />
+    </div>
+  );
+}
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import ResultBox from "../components/ResultBox";
+import { AuthContext } from "../contexts/AuthConext";
 
 export default function ResultsPage() {
-  return <div className="results-page">THESE ARE YOUR ELECTION RESULTS</div>;
+  const {
+    user: { elections },
+  } = useContext(AuthContext);
+  return (
+    <div className="results-page container">
+      <h3 className="container-title">Here are your election results:</h3>
+      {elections.map((election) => (
+        <ResultBox election={election} />
+      ))}
+    </div>
+  );
 }
